1e3c0ddcc0fa7be3d1fc8e67c87ef52006b2275a
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-26 16:57:09 -0600

    start with index page

---
+++ src/components/Home/About.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <article className='innerFlex aboutLink'>ABOUT</article>
+    )
+  }
+}
---
+++ src/components/Home/Blog.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Blog extends Component {
+  render() {
+    return (
+      <article className='innerFlex blogLink'>BLOG</article>
+    )
+  }
+}
+
+export default Blog;
