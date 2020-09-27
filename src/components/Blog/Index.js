cb5f4d599fb81511218f26ae00043f9f765caf71
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-27 09:20:24 -0600

    background and text

--- src/components/Blog/Index.js
+++ src/components/Blog/Index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import './blog.scss'
 
 class Index extends Component {
   render() {
     return (
-      <div>
-        blog
-      </div>
+      <article className='blogHome'>
+        <div className='blogTitle'>
+          I've read a lot of blogs but I don't have a favorite.
+          Sometimes I bookmark a good blog,
+          but if I need it again I certainly won't find it.
+
+          Writing a blog and doing more research on something helps me
+          to better understand and learn.
+
+          This section is a mix of Blogs, Tutorials, and Information.
+        </div>
+      </article>
     )
   }
 }
