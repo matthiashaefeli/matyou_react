dbf17c8104282622ce0a982a93b314e3f77c8e92
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-28 08:18:12 -0600

    title as component

--- src/components/Blog/Index.js
+++ src/components/Blog/Index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import './blog.scss';
-import Title from './Title';
+import Title from '../Title/Title';
 import Blog from './Blog';
 
 class Index extends Component {
@@ -28,27 +28,30 @@ class Index extends Component {
         }
       )
   }
+
+  titleText = "I've read a lot of blogs but I don't have a favorite. Sometimes I bookmark a good blog, but if I need it again I certainly won't find it. Writing a blog and doing more research on something helps me to better understand and learn. This section is a mix of Blogs, Tutorials, and Information."
+
   render() {
     const { error, isLoaded, blogs } = this.state;
 
     if (error) {
       return (
         <article className='blogHome'>
-          <Title />
+          <Title title={this.titleText} color={'white'} />
           <div>Error: {error.message}</div>
         </article>
       )
     } else if (!isLoaded) {
       return (
         <article className='blogHome'>
-          <Title />
+          <Title title={this.titleText} color={'white'} />
           <div>Loading data .....</div>
         </article>
       )
     } else {
       return (
         <article className='blogHome'>
-          <Title />
+          <Title title={this.titleText} color={'white'} />
           <div className='blogContainer'>
             {blogs.map(blog => (
               <Blog key={blog.id} blog={blog} />
--- src/components/Challenge/Index.js
+++ src/components/Challenge/Index.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Title from '../Title/Title';
+import './challenge.scss';
 
 class Index extends Component {
+  titletext = "Challenge page"
+
   render() {
     return (
-      <div>
-        Challenges
-      </div>
+      <article className='challengeHome'>
+        <Title title={this.titletext} color={'black'} />
+      </article>
     )
   }
 }
