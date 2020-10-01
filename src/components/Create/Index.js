be7724721040be49361e52164dc73a8befe81dd5
matthias haefeli <haefeli.matthias@gmail.com>
2020-10-01 14:13:07 -0600

    file upload and create note works

--- src/components/Create/Index.js
+++ src/components/Create/Index.js
@@ -3,6 +3,7 @@ import Back from '../Back/Back';
 import './create.scss';
 import Title from '../Title/Title';
 import CreateForm from './CreateForm';
+import CreateFormAttachment from './CreateFormAttachment';
 
 export default class Index extends Component {
   titleText = 'Create'
@@ -14,6 +15,7 @@ export default class Index extends Component {
         <Title title={this.titleText} color={'black'} />
         <CreateForm title='Blog' type='blog' sendUrl='https://matyou-api.herokuapp.com/blog' />
         <CreateForm title='Challenge' type='challenge' sendUrl='https://matyou-api.herokuapp.com/challenge' />
+        <CreateFormAttachment />
       </article>
     )
   }
