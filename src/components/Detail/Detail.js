5e48400622df61860098ee3ef78805cfd6da05cf
matthias haefeli <haefeli.matthias@gmail.com>
2020-10-02 08:17:24 -0600

    add trash

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import Cookie from 'js-cookie';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import './detail.scss';
 
-export default class Detail extends Component {
+
+class Detail extends Component {
+
   render() {
-    const { title, description, url } = this.props.detail
+    const { title, description, url } = this.props.detail;
+
+    let deleteLink
+    if (Cookie.get('token')) {
+      deleteLink = <FontAwesomeIcon icon={faTrash} className='deleteLink' />
+    } else {
+      deleteLink = null
+    }
 
     return (
-      <div className='detail' onClick={() => window.open(url, "_blank")}>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </div>
+      <>
+        <div className='detail' onClick={() => window.open(url, "_blank")}>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </div>
+        {deleteLink}
+      </>
     )
   }
 }
+
+export default Detail;
--- src/components/Detail/DetailSimple.js
+++ src/components/Detail/DetailSimple.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import Cookie from 'js-cookie';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import './detail.scss'
 
 export default class DetailSimple extends Component {
   render() {
     const { title, url } = this.props.detail
+
+    let deleteLink
+    if (Cookie.get('token')) {
+      deleteLink = <FontAwesomeIcon icon={faTrash} className='deleteLink' />
+    } else {
+      deleteLink = null
+    }
+
     return (
-      <div className='detailSimple' onClick={() => window.open(url, "_blank")}>
-        <h1>{title}</h1>
-      </div>
+      <>
+        <div className='detailSimple' onClick={() => window.open(url, "_blank")}>
+          <h1>{title}</h1>
+        </div>
+        {deleteLink}
+      </>
     )
   }
 }
