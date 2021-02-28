4fe70f20bb49fefd156c63be4248c9d677793421
matthiashaefeli <haefeli.matthias2gmail.com>
2021-02-28 11:56:45 -0600

    blog refactor

---
+++ src/components/Blog/blog.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+import './blog.scss';
+import Detail from '../Detail/Detail';
+import Loading from '../Loading/Loading';
+import NetworkError from '../../Network/Index';
+
+class Blog extends Component {
+  state = {
+    error: null,
+    isLoaded: false,
+    blogs: [],
+  }
+
+  componentDidMount = () => {
+    axios.get('https://matyou-api.herokuapp.com/blog')
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            blogs: result.data
+          })
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
+
+  render() {
+    const { error, isLoaded, blogs } = this.state;
+
+    if (error) {
+      return <NetworkError />
+    }
+
+    if (!isLoaded) {
+      return <Loading />
+    }
+
+    return (
+      <>
+        <p className='blogLength'>{this.state.blogs.length} Blogs</p>
+        <div className='blogContainer'>
+          {blogs.map(blog => (
+            <Detail key={blog.id} detail={blog} type='blog' />
+          ))}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Blog;
